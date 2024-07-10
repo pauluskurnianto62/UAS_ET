166c80ee1ef10f31763c99ed1df8f11bdd55f612
lukasyato <48988810+lukasyato@users.noreply.github.com>
2024-07-11 02:58:58 +0700

    fix home login

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,20 @@ String active_user = "";
 String _user_name = "Paulus Kurnianto";
 
 Future<String> checkUser() async {
-    final prefs = await SharedPreferences.getInstance();
-    String user_id = prefs.getString("user_id") ?? '';
-    return user_id;
-  }
+  final prefs = await SharedPreferences.getInstance();
+  String userId = prefs.getString("user_id") ?? '';
+  _user_name = prefs.getString("user_name") ?? '';
+  return userId;
+}
 
-  
-  
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   checkUser().then((String result) {
-    if (result == '')
-      runApp(MyLogin());
-    else {
+    if (result == '') {
+      runApp(const MyLogin());
+    } else {
       active_user = result;
-      runApp(MyApp());
+      runApp(const MyApp());
     }
   });
 }
@@ -36,14 +35,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 183, 58, 58)),
         useMaterial3: true,
       ),
       routes: {
-  			'adopt': (context) => Adopt(),
-        'browse':(context) => Browse(),
-        'offer':(context) => Offer(),
-        },
+        'adopt': (context) => Adopt(),
+        'browse': (context) => Browse(),
+        'offer': (context) => Offer(),
+      },
       home: const MyHomePage(title: 'Adopsian'),
     );
   }
@@ -58,7 +58,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-String _user_id = "";
+  String _user_id = "";
 
   @override
   void initState() {
@@ -84,43 +84,42 @@ String _user_id = "";
 
   Widget funDrawer() {
     return Drawer(
-        elevation: 16.0,
-        child: Column(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-                accountName: Text(_user_name),
-                accountEmail: Text(_user_id),
-                currentAccountPicture: CircleAvatar(
-                    backgroundImage:
-                        NetworkImage("https://i.pravatar.cc/150"))),
-            ListTile(
-              title: Text("Adopt"),
-              leading: Icon(Icons.pets),
-              onTap: () {
-                Navigator.popAndPushNamed(context, "about");
-            }),
-            ListTile(
-              title: Text("Offer"),
-              leading: Icon(Icons.front_hand),
-              onTap: () {
-                Navigator.popAndPushNamed(context, "studentlist");
-            }),
-            ListTile(
-              title: Text("Browse"),
-              leading: Icon(Icons.search),
-              onTap: () {
-                goBrowse();
-                Navigator.popAndPushNamed(context, "week3");
-            }),
-            ListTile(
-              title: Text("Logout"),
-              leading: Icon(Icons.logout),
+      elevation: 16.0,
+      child: Column(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+              accountName: Text(_user_name),
+              accountEmail: Text(_user_id),
+              currentAccountPicture: const CircleAvatar(
+                  backgroundImage: NetworkImage("https://i.pravatar.cc/800"))),
+          // ListTile(
+          //     title: const Text("Adopt"),
+          //     leading: const Icon(Icons.pets),
+          //     onTap: () {
+          //       Navigator.popAndPushNamed(context, "adopt");
+          //     }),
+          // ListTile(
+          //     title: const Text("Offer"),
+          //     leading: const Icon(Icons.handshake),
+          //     onTap: () {
+          //       Navigator.popAndPushNamed(context, "offer");
+          //     }),
+          // ListTile(
+          //     title: const Text("Browse"),
+          //     leading: const Icon(Icons.search),
+          //     onTap: () {
+          //       goBrowse();
+          //       Navigator.popAndPushNamed(context, "browse");
+          //     }),
+          ListTile(
+              title: const Text("Logout"),
+              leading: const Icon(Icons.logout),
               onTap: () {
                 doLogout();
-            })
-          ],
-        ),
-      );
+              })
+        ],
+      ),
+    );
   }
 
   @override
@@ -130,9 +129,78 @@ String _user_id = "";
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      drawer: funDrawer(),
+      body: SingleChildScrollView(
         child: Column(
-
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Card(
+              color: Colors.green[100],
+              child: InkWell(
+                onTap: () {
+                  Navigator.popAndPushNamed(context, "adopt");
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Adopt",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    SizedBox(
+                      width: 12,
+                      height: 72,
+                    ),
+                    Icon(Icons.pets)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.red[100],
+              child: InkWell(
+                onTap: () {
+                  Navigator.popAndPushNamed(context, "offer");
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Offer",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    SizedBox(
+                      width: 12,
+                      height: 72,
+                    ),
+                    Icon(Icons.handshake)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue[100],
+              child: InkWell(
+                onTap: () {
+                  Navigator.popAndPushNamed(context, "browse");
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Browse",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    SizedBox(
+                      width: 12,
+                      height: 72,
+                    ),
+                    Icon(Icons.search)
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,6 +6,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class MyLogin extends StatelessWidget {
+  const MyLogin({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,12 +15,14 @@ class MyLogin extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Login(),
+      home: const Login(),
     );
   }
 }
 
 class Login extends StatefulWidget {
+  const Login({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return _LoginState();
@@ -27,10 +31,7 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   String _user_id = '';
-  String _user_password='';
-
-  String error_login='';
-
+  String _user_password = '';
   void doLogin() async {
     final response = await http.post(
         Uri.parse("https://ubaya.me/flutter/160421074/login.php"),
@@ -43,90 +44,102 @@ class _LoginState extends State<Login> {
         prefs.setString("user_name", json['user_name']);
         main();
       } else {
-        setState(() {
-          error_login = "Incorrect user or password";
-        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Incorrect user or password')),
+        );
       }
     } else {
       throw Exception('Failed to read API');
     }
   }
-  
+
+  void doRegister() async {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Container(
-        height: 300,
-        margin: EdgeInsets.all(20),
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(30)),
-          border: Border.all(width: 1),
-          color: Colors.white,
-          boxShadow: [BoxShadow(blurRadius: 20)]),
-          child: Column(children: [	Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                onChanged: (v) {
-                  _user_id = v;
-                },
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Username',
-                    hintText: 'Enter valid username id'),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                onChanged: (v) {
-                       _user_password = v;
-                       },
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter secure password'),
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.all(20),
-                child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [Container(
-                  height: 50,
-                  width: 250,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20)),
-                  child: ElevatedButton(
-                    onPressed: () {doLogin();},
-                    child: Text(
-                      'Login',
-                      style: TextStyle(color: Colors.black, fontSize: 25),
-                    ),
-                  ),
+        appBar: AppBar(
+          title: const Text('Login'),
+        ),
+        body: Container(
+            height: 300,
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(30)),
+                border: Border.all(width: 1),
+                color: Colors.white,
+                boxShadow: const [BoxShadow(blurRadius: 20)]),
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  onChanged: (v) {
+                    _user_id = v;
+                  },
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
+                      hintText: 'Enter valid username id'),
                 ),
-                Container(
-                  height: 50,
-                  width: 250,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20)),
-                  child: ElevatedButton(
-                    onPressed: () {doLogin();},
-                    child: Text(
-                      'Register',
-                      style: TextStyle(color: Colors.black, fontSize: 25),
-                    ),
-                  ),
-                )]
-		        ),
-          )
-        ])
-      )
-    );
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                //padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  onChanged: (v) {
+                    _user_password = v;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter secure password'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 50,
+                          width: 250,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              doLogin();
+                            },
+                            child: const Text(
+                              'Login',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 25),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 50,
+                          width: 250,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              doRegister();
+                            },
+                            child: const Text(
+                              'Register',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 25),
+                            ),
+                          ),
+                        ),
+                      )
+                    ]),
+              )
+            ])));
   }
 }
